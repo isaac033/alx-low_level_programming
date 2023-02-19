c332e056e9e40f322cd7d61ea31f7a93ccb4f4fd
isaac033 <isaacwangari15@gmail.com>
2023-02-19 07:20:09 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - entry code 
+ *
+ * return : always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char c = 'z';
+	while 
+		(c >= 'a')
+		{
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
