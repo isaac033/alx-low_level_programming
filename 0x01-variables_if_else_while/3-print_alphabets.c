264981dd0900cc73de28528124c39d00b8031e60
isaac033 <isaacwangari15@gmail.com>
2023-02-19 05:51:06 -0800

    taask 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ *
+ * main - entry code
+ *
+ * return: always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int ch = 'a';
+	int CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+	return (0);
+}
