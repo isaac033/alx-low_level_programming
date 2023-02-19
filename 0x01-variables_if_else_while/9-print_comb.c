9a0f711424dd77ad7eb0964270d75ded1257bd41
isaac033 <isaacwangari15@gmail.com>
2023-02-19 01:31:43 -0800

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - put your code here
+ *
+ * return:Always 0 (success)
+ *
+ */
+
+
+int main (void)
+	{
+	
+		int a;
+		for (a=0; a<10; a++)
+		{
+			putchar(a+'0');
+			if (a<9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+		return 0;
+
+	}
+
