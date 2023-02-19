01559c3d3adc2483aaf6ee47cc30b3e7c32e95aa
isaac033 <isaacwangari15@gmail.com>
2023-02-19 06:57:23 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ *
+ * main - entry code
+ */
+
+int main(void)
+{
+	char ch = 'a';
+	while (ch <= 'z')
+	{
+		if ((ch != 'e') && (ch != 'q'))
+		{
+			putchar(ch);
+			ch++;
+		}
+	}
+		putchar('\n');
+		return (0);
+}
+
+
+				
+
