99ffccc43fae0e2e126b83123eb5ce9683856594
isaac033 <isaacwangari15@gmail.com>
2023-02-19 07:14:32 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ *
+ * main - entry code 
+ *
+ * return : always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int c;
+	c = 0;
+	while 
+		(c < 10)
+		{
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
+
