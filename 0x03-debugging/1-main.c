162a0105b80151cb305a0a6c087e1720c6f6f445
isaac033 <isaacwangari15@gmail.com>
2023-02-23 00:41:20 -0800

    task 0

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ * return : 0
+ */
+int main(void)
+{
+	int i;
+	printf("Infinite loop incoming:(\n");
+
+	i = 0;
+	/**
+	 * while (i < 10)
+	 * {
+	 * 	putchar(i);
+	 * }
+	 */
+printf ("Infinite lood avoided! \\0/\n");	
+return (0);
+}
