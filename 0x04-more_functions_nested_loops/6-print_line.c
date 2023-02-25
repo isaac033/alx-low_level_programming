c127aa049190072b91b6a00e3626e95823a3f23f
isaac033 <isaacwangari15@gmail.com>
2023-02-24 18:27:23 -0800

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,19 +7,21 @@
  */
 void print_line(int n)
 {
- if (n <= 0)
- {
- _putchar('\n');
- } else
- {
- int i;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
 
- for (i = 1; i <= n; i++)
- {
- _putchar('_');
- }
- _putchar('\n');
- }
+{
+int i;
+
+for (i = 1; i <= n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
 
 }
 
