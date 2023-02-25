ea57ac9f7976fa90f9717d2e6df82f0454beff70
isaac033 <isaacwangari15@gmail.com>
2023-02-25 03:04:02 -0800

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,29 +1,22 @@
-#include<stdio.h>
-/**
- * main - entry code
- *
- * return: always 0 (success
- *
- */
+#include <stdio.h>
 
+/**
+ * main - Prints numbers between 0 to 9 and letters between a to f.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char c = 'a';
-	int d = '0';
+	int i;
 
-	while 
-		(d < 10)
-		{
-			putchar(d + '0');
-			d++;
-		}
-	while 
-		(c <= 'f')
-		{
-			putchar(c);
-			c++;
-		}
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
 	putchar('\n');
 	return (0);
 }
-
